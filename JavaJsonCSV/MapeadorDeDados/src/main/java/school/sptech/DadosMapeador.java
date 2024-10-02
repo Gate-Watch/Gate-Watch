88bf8dadb9511a4e90fb12ad018df101ecbd531e
JVictorDZ <joao.silva998@sptech.school>
2024-10-02 10:58:28 -0300

    Solução de problemas em data e hora

--- JavaJsonCSV/MapeadorDeDados/src/main/java/school/sptech/DadosMapeador.java
+++ JavaJsonCSV/MapeadorDeDados/src/main/java/school/sptech/DadosMapeador.java
@@ -2,6 +2,7 @@ package school.sptech;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,6 +11,7 @@ import java.util.List;
 public class DadosMapeador {
     public List<Dado> mapearDado(InputStream inputStream) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
         List<Dado> dados = objectMapper.readValue(inputStream, new TypeReference<List<Dado>>() {
         });
         return dados;
