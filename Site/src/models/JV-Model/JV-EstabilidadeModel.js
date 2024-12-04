426b11b37eebf2954710326bb76372a8a04c6ac1
JVictorDZ <joao.silva998@sptech.school>
2024-12-04 11:44:58 -0300

    Inclusão de desvio padrão na estabilidade

--- Site/src/models/JV-Model/JV-EstabilidadeModel.js
+++ Site/src/models/JV-Model/JV-EstabilidadeModel.js
@@ -2,15 +2,20 @@ const database = require("../../database/config");
 
 async function calcularEstabilidade() {
     const instrucaoGeral = `
-        SELECT 
-            AVG(cpu_usage) AS media_cpu_usage,
-            AVG(memory_perc) AS media_memory_usage,
-            AVG(disk_perc) AS media_disk_usage
-        FROM 
-            Desempenho
-        WHERE 
-            MONTH(dtHora) = MONTH(CURRENT_DATE)
-            AND YEAR(dtHora) = YEAR(CURRENT_DATE);
+    SELECT 
+    AVG(cpu_usage) AS media_cpu_usage,
+    STDDEV(cpu_usage) AS desvio_padrao_cpu_usage,
+    AVG(memory_perc) AS media_memory_usage,
+    STDDEV(memory_perc) AS desvio_padrao_memory_usage,
+    AVG(disk_perc) AS media_disk_usage,
+    STDDEV(disk_perc) AS desvio_padrao_disk_usage
+FROM 
+    Desempenho
+WHERE 
+    MONTH(dtHora) = MONTH(CURRENT_DATE)
+    AND YEAR(dtHora) = YEAR(CURRENT_DATE);
+
+
     `;
 
     try {
